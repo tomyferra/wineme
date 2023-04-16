8428513ac358c5bf4bd3b45d4ac159363db712e8
tomyferra <ferra.tomy@gmail.com>
2023-04-16 01:23:43 -0300

    removed loader

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://wineme-api.onrender.com",
+  baseURL: "https://wineme-api.onrender.com/api/wines",
   headers: {
     "Content-type": "application/json"
   }
