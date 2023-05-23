51c490d2d4b46545582a8bea7f67fd76c07b0d82
tomyferra <ferra.tomy@gmail.com>
2023-05-22 23:39:10 -0300

    changed endpoint

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://wineme-api.onrender.com/api/wines",
-  //baseURL: "https://wineme-api.vercel.app/api/wines",
+  //baseURL: "https://wineme-api.onrender.com/api/wines",
+  baseURL: "https://wineme-api.vercel.app/api/wines",
   headers: {
     "Content-type": "application/json"
   }
