4205ca124101ba45ba0c21a3f2a1c591a78f0d17
tomyferra <ferra.tomy@gmail.com>
2025-01-03 12:16:08 -0300

    changing url

--- src/services/api-login.js
+++ src/services/api-login.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 // Leer variables de entorno
-const baseURL = process.env.REACT_APP_API_BASE_URL;
+const loginUrl = process.env.REACT_APP_API_LOGIN_URL;
 const email = process.env.REACT_APP_API_USER_EMAIL;
 const password = process.env.REACT_APP_API_USER_PASSWORD;
 
 const login = async () => {
   try {
     const response = await axios.post(
-      `${baseURL}user/login`,
+      loginUrl,
       { email: email, password: password },
       {
         headers: {
